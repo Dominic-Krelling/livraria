6ad76b5adf8e734332aeea82b3076ec40ce3f173
Raphael-Otto <Raphaelotto.RO@gmail.com>
2023-03-07 14:56:49 -0300

    criação da model editora

--- livraria/admin.py
+++ livraria/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Categoria
+from .models import Categoria, Editora
 
 admin.site.register(Categoria)
+admin.site.register(Editora)
--- livraria/models.py
+++ livraria/models.py
@@ -2,3 +2,13 @@ from django.db import models
 
 class Categoria(models.Model):
     descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.descricao} ({self.id})"
+
+class Editora(models.Model):
+    nome = models.CharField(max_length=100)
+    site = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
