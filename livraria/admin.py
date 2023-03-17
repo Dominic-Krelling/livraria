82a740d704c3cf1d79c3b9384bcebfea95ee8618
Dominic-Krelling <dominic.krelling1@gmail.com>
2023-03-17 14:33:33 -0300

    Criação do Autor e Livro

--- livraria/admin.py
+++ livraria/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Categoria, Editora
+from .models import Categoria, Editora, Autor, Livro
 
+admin.site.register(Autor)
 admin.site.register(Categoria)
 admin.site.register(Editora)
+admin.site.register(Livro)
