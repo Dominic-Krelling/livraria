0a646e2c2b9ea604074379d95d939ad2d4a99891
Dominic-Krelling <dominic.krelling1@gmail.com>
2023-04-04 14:25:49 -0300

    instalando DRF - opções de manipulação do bd

--- livraria/views.py
+++ livraria/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+
+from livraria.models import Categoria
+from livraria.serializers import CategoriaSerializer
+
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
