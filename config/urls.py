91daeeea04f2727aeb9bdc98214220317c2c72c1
Dominic-Krelling <dominic.krelling1@gmail.com>
2023-04-11 14:55:48 -0300

    adição do editora,autor e livro ao django rest_framework

--- config/urls.py
+++ config/urls.py
@@ -4,10 +4,13 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from livraria.views import CategoriaViewSet
+from livraria.views import CategoriaViewSet, EditoraViewSet,LivroViewSet, AutorViewSet
 
 router = DefaultRouter()
 router.register(r'categorias', CategoriaViewSet)
+router.register(r'editora', EditoraViewSet)
+router.register(r'autor', AutorViewSet)
+router.register(r'editora', EditoraViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- livraria/views.py
+++ livraria/views.py
@@ -2,9 +2,25 @@ from django.shortcuts import render
 
 from rest_framework.viewsets import ModelViewSet
 
-from livraria.models import Categoria
-from livraria.serializers import CategoriaSerializer
+from livraria.models import Categoria, Editora, Autor, Livro
+from livraria.serializers import CategoriaSerializer, EditoraSerializer, AutorSerializer, LivroDetailSerializer, LivroSerializer
 
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
+
+class EditoraViewSet(ModelViewSet):
+    queryset = Editora.objects.all()
+    serializer_class = EditoraSerializer
+
+class AutorViewSet(ModelViewSet):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
+
+class LivroViewSet(ModelViewSet):
+    queryset = Livro.objects.all()
+
+    def get_serializer_class(self):
+        if self.action in ["list", "retrieve"]:
+            return LivroDetailSerializer
+        return LivroSerializer
