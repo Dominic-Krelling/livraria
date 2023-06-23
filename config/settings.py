04bad6eaf3bca8d5a2a65ef9e166554f4e8fcf68
Dominic-Krelling <dominic.krelling1@gmail.com>
2023-06-23 14:28:09 -0300

    permissoes

--- config/settings.py
+++ config/settings.py
@@ -128,8 +128,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
 
-REST_FRAMEWORK = {
-    "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated",
-    ]
-}
+# REST_FRAMEWORK = {
+#     "DEFAULT_PERMISSION_CLASSES": [
+#         "rest_framework.permissions.IsAuthenticated",
+#     ]
+# }
