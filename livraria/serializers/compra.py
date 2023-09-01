e73b59eb7411fb98b25bf7ad0d3161d8f06f08c5
Dominic-Krelling <dominic.krelling1@gmail.com>
2023-09-01 15:12:00 -0300

    28 p1

--- livraria/serializers/compra.py
+++ livraria/serializers/compra.py
@@ -1,10 +1,17 @@
 from rest_framework.serializers import ModelSerializer, CharField
 
-from livraria.models import Compra
+from livraria.models import Compra, ItensCompra
+
+class ItensCompraSerializer(ModelSerializer):
+    class Meta:
+        model = ItensCompra
+        fields = ["livro", "quantidade"]
+        depth = 2
 
 class CompraSerializer(ModelSerializer):
     class Meta:
-        model = Compra
-        fields = "__all__"
         usuario = CharField(source="usuario.email", read_only=True)
         status = CharField(source="get_status_display", read_only=True)
+        model = Compra
+        fields = "__all__"
+        itens = ItensCompraSerializer(many=True, read_only=True)
